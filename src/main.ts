f554eab921cbd5638efa979a022fd844ae9edab6
giacomotolari <tolarigiacomo@gmail.com>
2025-01-21 10:12:04 +0100

    format test 4

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import "./css/index.css";
 // import typescriptLogo from './typescript.svg'
 // import viteLogo from '/vite.svg'
 
-                        import { navigation } from "./features/navigation/navigation";
-                import { initRouter } from "./features/navigation/router";
+import { navigation } from "./features/navigation/navigation";
+import { initRouter } from "./features/navigation/router";
 
 const appEl = document.querySelector<HTMLDivElement>("#app");
 
