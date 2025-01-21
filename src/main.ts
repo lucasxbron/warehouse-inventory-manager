6c1dae4cc6f761264e2fe1c543e1d6869807c72f
giacomotolari <tolarigiacomo@gmail.com>
2025-01-21 10:00:30 +0100

    format

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./css/index.css";
 // import typescriptLogo from './typescript.svg'
 // import viteLogo from '/vite.svg'
 
-import { navigation } from "./features/navigation/navigation";
+        import { navigation } from "./features/navigation/navigation";
 import { initRouter } from "./features/navigation/router";
 
 const appEl = document.querySelector<HTMLDivElement>("#app");
